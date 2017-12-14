51eb7d57bf385db5c87810d635287fc4c605c3a9
crew@kashyak.com <venkatanagarajas@gmail.com>
2017-12-14 19:03:03 +0530

    Authnitication With JWT

--- visitservice.go
+++ visitservice.go
@@ -1,11 +1,19 @@
 package visitservice
 
 import (
+	"crypto/rsa"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
@@ -38,7 +46,36 @@ type SuccessResponse struct {
 	Message string `json:"message"`
 }
 
+//User is a strct
+type User struct {
+	Username string `datastore:"-"`
+	Password string `json:"password"`
+}
+
+// Response is strcut
+type Response struct {
+	Data string `json:"data"`
+}
+
+// Token is also strcut
+type Token struct {
+	Token string `json:"token"`
+}
+
+const (
+	privKeyPath = "keys/app.rsa"
+	pubKeyPath  = "keys/app.rsa.pub"
+)
+
+var (
+	verifyKey *rsa.PublicKey
+	signKey   *rsa.PrivateKey
+)
+var verifyBytes, signBytes []byte
+
 func init() {
+	initKeys()
+	startServer()
 	http.HandleFunc("/api/getallvisits/", restHandler)
 	http.HandleFunc("/api/postavisit/", restHandler)
 	http.HandleFunc("/api/deleteavisit/", restHandler)
@@ -59,6 +96,183 @@ func restHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		//respondErr(w, r, http.StatusNotFound, "is not supported HTTP methods")
 	}
+}
+func initKeys() {
+	var err error
+
+	signBytes, err = ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		log.Fatalf("Error reading private key: %v", err)
+	}
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		log.Fatalf("Error parsing private key: %v", err)
+	}
+	verifyBytes, err = ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		log.Fatalf("Error reading public key: %v", err)
+	}
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	if err != nil {
+		log.Fatalf("Error parsing public key: %v", err)
+	}
+}
+
+// ProtectedHandler is handler
+/*func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+
+	response := Response{"Gained access to protected resource"}
+	JSONResponse(response, w)
+
+}*/
+
+//RegisterHandler is using to insert user into database
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+
+	user := &User{}
+	if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+		panic(err)
+	}
+
+	keys := make([]*datastore.Key, 1)
+	ctx := appengine.NewContext(r)
+	ctx, err := appengine.Namespace(ctx, NAMESPACENAME)
+	if err != nil {
+		panic(err)
+	}
+
+	keys[0] = datastore.NewKey(ctx, "User", user.Username, 0, nil)
+
+	_, err = datastore.Put(ctx, keys[0], user)
+	if err != nil {
+		panic(err)
+	}
+
+	//user.ID = k.IntID()
+	json.NewEncoder(w).Encode(user)
+}
+
+//LoginHandler is using to check wherther user existed or not
+//If User Existed In the datastore send tocken back
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	user := &User{}
+	if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+		panic(err)
+	}
+	ctx := appengine.NewContext(r)
+	ctx, err := appengine.Namespace(ctx, NAMESPACENAME)
+	if err != nil {
+		panic(err)
+	}
+
+	// get one
+	//user := &User{}
+	key := datastore.NewKey(ctx, "User", user.Username, 0, nil)
+	err = datastore.Get(ctx, key, user)
+	if err != nil {
+		// there is an err, there is a NO user
+		//fmt.Fprint(w, "false")
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Println("Error logging in")
+		fmt.Fprint(w, "Invalid credentials")
+		return
+	}
+
+	//create a rsa 256 signer
+	signer := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"iss": "admin",
+		"exp": time.Now().Add(time.Minute * 20).Unix(),
+		"CustomUserInfo": struct {
+			Name string
+			Role string
+		}{user.Username, "Member"}})
+
+	//set claims
+
+	tokenString, err := signer.SignedString(signKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error while signing the token")
+		log.Printf("Error signing token: %v\n", err)
+	}
+	//create a token instance using the token string
+	//response := Token{tokenString}
+	cookie := &http.Cookie{
+		Name:  "session",
+		Value: tokenString,
+		Path:  "/",
+		//		UNCOMMENT WHEN DEPLOYED:
+		//		Secure: true,
+		//		HttpOnly: true,
+	}
+	http.SetCookie(w, cookie)
+
+	//JsonResponse(response, w)
+
+}
+
+//ValidateTokenMiddleware is a AUTH TOKEN VALIDATION
+func ValidateTokenMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+	//validate token
+	token, err := request.ParseFromRequest(r, request.OAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
+		return verifyKey, nil
+	})
+
+	if err == nil {
+
+		if token.Valid {
+			next(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Token is not valid")
+		}
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Unauthorised access to this resource")
+	}
+
+}
+
+//JSONResponse is a HELPER FUNCTIONS
+func JSONResponse(response interface{}, w http.ResponseWriter) {
+
+	json, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func startServer() {
+	http.HandleFunc("/register", RegisterHandler)
+	http.HandleFunc("/login", LoginHandler)
+
+	//PROTECTED ENDPOINTS
+	/*	http.Handle("/resource/", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(ProtectedHandler)),
+	))*/
+
+	http.Handle("/api/getallvisits/", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(restHandler)),
+	))
+	http.Handle("/api/postavisit/", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(restHandler)),
+	))
+	http.Handle("/api/deleteavisit/", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(restHandler)),
+	))
+
+	log.Println("Now listening...")
+
 }
 
 func getallvisitshandler(w http.ResponseWriter, r *http.Request) {
